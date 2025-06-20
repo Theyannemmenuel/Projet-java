cfff51a7bf2b3e22d62a1f8521143b8a4925f33f
Yann Amangoua <yannamangoua31@gmail.com>
2025-06-20 09:11:14 +0200

    moteur v2

--- src/moteur/GestionTour.java
+++ src/moteur/GestionTour.java
@@ -1,5 +1,28 @@
-package src.moteur;
+package moteur;
 
 public class GestionTour {
-    
+    private int indexActuel;
+    private int sens = 1; // 1 = horaire, -1 = antihoraire
+    private final int nbJoueurs;
+
+    public GestionTour(int nbJoueurs) {
+        this.nbJoueurs = nbJoueurs;
+        this.indexActuel = 0;
+    }
+
+    public void tourSuivant() {
+        indexActuel = (indexActuel + sens + nbJoueurs) % nbJoueurs;
+    }
+
+    public void inverserSens() {
+        sens *= -1;
+    }
+
+    public int getIndexActuel() {
+        return indexActuel;
+    }
+
+    public int getProchainIndex() {
+        return (indexActuel + sens + nbJoueurs) % nbJoueurs;
+    }
 }
--- src/moteur/MoteurJeu.java
+++ src/moteur/MoteurJeu.java
@@ -1,5 +1,90 @@
-package src.moteur;
+package moteur;
+
+import cartes.Carte;
+import jeu.Jeu;
+import joueur.Joueur;
+import utils.TypeAction;
+
+import java.util.List;
 
 public class MoteurJeu {
-    
+    private final Jeu jeu;
+    private final GestionTour gestionTour;
+
+    public MoteurJeu(List<Joueur> joueurs) {
+        this.jeu = new Jeu(joueurs);
+        this.gestionTour = new GestionTour(joueurs.size());
+    }
+
+    public void demarrerPartie() {
+        boolean partieTerminee = false;
+
+        while (!partieTerminee) {
+            Joueur joueur = jeu.getJoueur(gestionTour.getIndexActuel());
+            Carte carteTalon = jeu.getTalon().getDerniereCarte();
+
+            System.out.println("\nTalon : " + carteTalon);
+            System.out.println("Tour de " + joueur.getNom());
+
+            Carte carteJouee = joueur.jouerCarte(carteTalon);
+
+            if (carteJouee != null) {
+                jeu.getTalon().ajouterCarte(carteJouee);
+                System.out.println(joueur.getNom() + " joue : " + carteJouee);
+                appliquerEffet(carteJouee);
+                if (joueur.getMain().isEmpty()) {
+                    System.out.println(joueur.getNom() + " a gagné !");
+                    partieTerminee = true;
+                    continue;
+                } else if (joueur.getMain().size() == 1) {
+                    System.out.println(joueur.getNom() + " dit UNO !");
+                }
+            } else {
+                Carte piochee = jeu.getPioche().piocher();
+                System.out.println(joueur.getNom() + " pioche une carte.");
+                if (piochee.estJouable(carteTalon)) {
+                    System.out.println("Elle est jouable et posée !");
+                    jeu.getTalon().ajouterCarte(piochee);
+                    appliquerEffet(piochee);
+                } else {
+                    joueur.ajouterCarte(piochee);
+                }
+            }
+
+            gestionTour.tourSuivant();
+        }
+    }
+
+    private void appliquerEffet(Carte carte) {
+        switch (carte.getTypeAction()) {
+            case PASSER -> {
+                System.out.println("Le prochain joueur passe son tour.");
+                gestionTour.tourSuivant();
+            }
+            case INVERSION -> {
+                System.out.println("Le sens du jeu est inversé !");
+                gestionTour.inverserSens();
+            }
+            case PLUS_DEUX -> {
+                System.out.println("Le prochain joueur pioche 2 cartes !");
+                Joueur suivant = jeu.getJoueur(gestionTour.getProchainIndex());
+                suivant.ajouterCarte(jeu.getPioche().piocher());
+                suivant.ajouterCarte(jeu.getPioche().piocher());
+                gestionTour.tourSuivant(); // Il passe son tour
+            }
+            case PLUS_QUATRE -> {
+                System.out.println("Le prochain joueur pioche 4 cartes !");
+                Joueur suivant = jeu.getJoueur(gestionTour.getProchainIndex());
+                for (int i = 0; i < 4; i++) {
+                    suivant.ajouterCarte(jeu.getPioche().piocher());
+                }
+                gestionTour.tourSuivant(); // Il passe son tour
+            }
+            case JOKER -> {
+                System.out.println("Carte joker jouée. Couleur à choisir par le joueur.");
+                // À compléter si choix de couleur par joueur
+            }
+            default -> {}
+        }
+    }
 }
